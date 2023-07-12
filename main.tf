5376a42baed8247bd5e4050aa8867090e0aa5033
Chabail <charles.bailleul01@gmail.com>
2023-07-12 11:10:27 +0200

    suppression bastion terraform et maj creds

--- main.tf
+++ main.tf
@@ -34,17 +34,7 @@ module "google_kubernetes_cluster" {
   master_ipv4_cidr_block     = module.google_networks.cluster_master_ip_cidr_range
   pods_ipv4_cidr_block       = module.google_networks.cluster_pods_ip_cidr_range
   services_ipv4_cidr_block   = module.google_networks.cluster_services_ip_cidr_range
-  authorized_ipv4_cidr_block = "${module.bastion.ip}/32"
+
 }
 
-# Uncomment to add bastion
-module "bastion" {
-  source = "./bastion"
 
-  project_id   = var.project_id
-  region       = var.region
-  zone         = var.main_zone
-  bastion_name = "app-cluster"
-  network_name = module.google_networks.network.name
-  subnet_name  = module.google_networks.subnet.name
-}
